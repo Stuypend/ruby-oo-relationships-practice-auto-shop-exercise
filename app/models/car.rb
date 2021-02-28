20ea7677951d1c2e506e51d892d982dfec3d82c7
stuypend <andre@stuypend.com>
2021-02-28 13:02:34 -0500

    Done.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,34 @@
 class Car
 
+  @@all = []
+  attr_accessor :classification, :owner
   attr_reader :make, :model
 
-  def initialize(make, model, classification)
+  def initialize(classification, owner, make=1, model=1)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    return @@all
+  end
+
+  def self.classification
+    return @@all.map do
+      |car|
+      car.classification
+    end
+  end
+
+  def self.find_mechanics(classification)
+    Mechanic.all.select { |mechanic| mechanic.specialty == self.classification }
   end
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,67 @@
+require_relative "car.rb"
+require_relative "car_owner.rb"
+
 class Mechanic
+
+  @@all = []
 
   attr_reader :name, :specialty
 
 def initialize(name, specialty)
   @name = name
   @specialty = specialty
+  save
 end
 
+  def save
+    @@all << self
+  end
+
+  def self.all
+    return @@all
+  end
+
+  def cars
+    return Car.all.select  {|car| car.classification == self.specialty}
+  end
+
+  def car_owners
+    return CarOwner.all.select  {|owner| owner.mechanics.include?(self)}
+  end
+
+  def car_owners_names
+    result = CarOwner.all.select  {|owner| owner.mechanics.include?(self)}
+    return result.map { |owner| owner.name }
+  end
 
 end
+
+o1 = CarOwner.new("Jeff")
+o2 = CarOwner.new("Jess")
+o3 = CarOwner.new("Jeeter")
+
+c1 = Car.new("Toyata", o1)
+c2 = Car.new("Toyata", o2)
+c3 = Car.new("Ford", o1)
+c4 = Car.new("GM", o3)
+c5 = Car.new("GM", o3)
+c5 = Car.new("GM", o1)
+
+m1 = Mechanic.new("x", "Toyata")
+m2 = Mechanic.new("a", "Ford")
+m3 = Mechanic.new("n", "GM")
+m4 = Mechanic.new("m", "GM")
+
+pp m1.cars
+puts
+pp m2.cars
+puts
+pp m3.cars
+puts
+
+pp m1.car_owners_names
+puts
+pp m2.car_owners_names
+puts
+pp m3.car_owners_names
+puts
